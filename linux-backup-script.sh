177cf3f78b363750d948a68b70b7d86334e824f1
MaksymilianMielcarski <maksmielcarski@gmail.com>
2025-05-25 13:56:51 +0200

    Updated script

--- linux-backup-script.sh
+++ linux-backup-script.sh
@@ -2,15 +2,17 @@
 
 source ~/.azbackup/.env
 
-TIMESTAMP=$(date+"%Y-%m-%d_%H-%M-%S")
-BACKUP_NAME ="backup-$TIMESTAMP.tar.gz"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_NAME="backup-$TIMESTAMP.tar.gz"
 SOURCE_DIR="/var/www/html"
 TMP_DIR="/tmp"
 BACKUP_PATH="$TMP_DIR/$BACKUP_NAME"
 
+echo "Crating backupfile..."
 tar -czf "$BACKUP_PATH" "$SOURCE_DIR"
 
-azcopy "$BACKUP_PATH" \ "https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$CONTAINER_NAME/$BACKUP_NAME?$STORAGE_ACCOUNT_KEY" \ --overwrite=true
+echo "Copying backup to Azure..."
+azcopy copy "$BACKUP_PATH" "https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net>
 
+echo "Deleting local backup file"
 rm "$BACKUP_PATH"
-
